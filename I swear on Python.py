7e9f4dbbbb43886a27e8b0180b7113748c299ae0
luselmann <66573161+luselmann@users.noreply.github.com>
2020-06-22 20:09:22 +0200

    code cleanup 3

--- I swear on Python.py
+++ I swear on Python.py
@@ -4,22 +4,15 @@ engine = pyttsx3.init()
 exclude = ('the', 'i', 'you', 'that', 'my', 'and', 'on', 'is', 'so', 'your', 'it', 'a', 'to', 'make', "i'm")
 counts = dict() #create dict
 
-# FUNCTIONS
-
-def word_count(words):
-    for word in words:
-        if word not in exclude:
-            if word in counts:
-                counts[word] += 1 #add value of word to dict
-            else:
-                counts[word] = 1 #add word to dict
-
-# PROGRAM START
-
 file = open('AssNTiddies.txt').read()
 perfectwords = file.lower().replace('.', '').replace(',', '').replace('\n', ' ').split()
 
-word_count(perfectwords)
+for word in perfectwords:
+    if word not in exclude:
+        if word in counts:
+            counts[word] += 1 #add value of word to dict
+        else:
+            counts[word] = 1 #add word to dict
 
 sorted_counts = sorted(counts.items(), key = lambda x: x[1], reverse = True) #sort by value/frequency, reverse it to put highest on top
 
