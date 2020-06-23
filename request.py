c54b7b9c060c0c7fe6e700ceb3abe78db77c3ff4
luselmann <66573161+luselmann@users.noreply.github.com>
2020-06-23 13:35:58 +0200

    added comments

--- request.py
+++ request.py
@@ -1,40 +1,42 @@
-import requests
-import json
-from bs4 import BeautifulSoup
+import requests #tool to request info from website
+import json #import to adjust to block format -> easy to read
+from bs4 import BeautifulSoup #tool to find div(lyrics element) in html/interact with html
+
+#FUNCTIONS
 
 def printJSON(s):
     print(json.dumps(s, indent = 4, sort_keys = True, ensure_ascii = False))
+    #print JSON format in alphabetical order and let apostrophes be used
 
 def getlyrics(song):
     headers = { 'Authorization': 'Bearer C0eD8lJNIEd7F6z4-5vW-7yYbV_PCcPBGDjluLChqg5BM-hP2x7vjg2ljBhCiYeg' }
+    #authorization for genius api
 
-    data = { 'q': song }
+    data = { 'q': song } #song data entered and searched for
 
     response = requests.get('https://api.genius.com/search', data = data, headers = headers).json()
+    #request for song results from genius api in JSON
 
-    hits = response['response']['hits']
+    hits = response['response']['hits'] #definition of hits for following conditional etc.
 
     if len(hits) < 1:
-        return 'no songs found'
+        return 'no songs found' #if less than 1(none) hit found -> no songs found
 
-    result = hits[0]['result']
-    # printJSON(result)
+    result = hits[0]['result'] #first result chosen automatically (taken from result JSON block)
 
-    url = result['url']
-    full_title = result['full_title']
-    printJSON(full_title)
+    url = result['url'] #defines url for first result (taken from url JSON block)
+    full_title = result['full_title'] #defines title and artist (taken from full_title JSON block)
+    printJSON(full_title) #prints title and artist in JSON format
 
-    page = requests.get(url)
+    page = requests.get(url) #finds page/url of song
 
-    print(page.status_code)
+    html = BeautifulSoup(page.text, 'html.parser') #finds elements in html
 
-    html = BeautifulSoup(page.text, 'html.parser')
-
-    div = html.find('div', { 'class': 'lyrics' })
+    div = html.find('div', { 'class': 'lyrics' }) #finds lyrics element
 
     if div is None:
-        return 'no lyrics found'
+        return 'no lyrics found' #if no lyrics available for the song -> no lyrics found
 
-    lyrics = div.get_text()
+    lyrics = div.get_text() #defines lyrics as text taken from correct div -> gets only text from div
 
-    return lyrics
+    return lyrics #returns and displays lyrics
